fdfc6d9cdd77383207a6f867b031d7597d2272a7
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-07-26 18:06:10 -0300

    model user y create

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,30 @@
-export const signup = (req, res) => {
-  console.log(req.body);
+import User from "../model/user.model.js";
+
+export const signup = async (req, res) => {
   try {
     const { fullname, username, email, password, confirmedPassword, gender } = req.body;
+
+    if (!fullname || !username || !email || !password || !confirmedPassword || !gender)
+      return res.status(400).json({ error: "All fields are required" });
+    if (password !== confirmedPassword) return res.status(400).json({ error: "Passwords do not match" });
+
+    const user = await User.findOne({ username });
+    if (user) return res.status(400).json({ error: "Username already exists" });
+    else {
+      const boysPic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+      const girlPic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+      const newUser = new User({
+        fullname,
+        username,
+        email,
+        password,
+        gender,
+        profilePic: gender === "male" ? boysPic : girlPic,
+      });
+      await newUser.save();
+      res.status(201).json({ message: "User created successfully", newUser: newUser });
+    }
   } catch (error) {
     console.log(error);
   }
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoute from "./routes/auth.routes.js";
+import conectToMongodb from "./db/connectDb.js";
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
 app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
+  conectToMongodb();
+
   console.log(`Server is running on port ${PORT}`);
 });
