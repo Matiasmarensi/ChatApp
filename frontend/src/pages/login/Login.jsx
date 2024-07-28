80393df40a54be8207540c3a2264b06e3d5ead95
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-07-28 12:44:37 -0300

    login start

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className=" flex flex-col items-center justify-center  min-w-96 mx-auto">
+      <div className="w-full  p-6 round-lg shadow-md bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-200">
+          Login<span className="text-sky-500"> ChatApp</span>
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
