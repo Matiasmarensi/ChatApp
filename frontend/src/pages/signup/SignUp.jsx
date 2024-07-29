13493de4d4816c85668abd9d793d85911ee446bc
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-07-28 22:08:03 -0300

    starting signup

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SignUp = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto  ">
+      <div className="w-full p-6 rounded-lg shadow-md"></div>
+    </div>
+  );
 };
 
 export default SignUp;
