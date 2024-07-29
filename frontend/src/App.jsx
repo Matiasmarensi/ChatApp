22b3c9d12cfd871a1b7addfcc4f1014710c1db6f
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-07-29 20:48:33 -0300

    signup starting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
 function App() {
   return (
     <div className=" h-screen flex items-center justify-center">
       <Login />
+      <SignUp />
     </div>
   );
 }
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const SignUp = () => {
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto  ">
-      <div className="w-full p-6 rounded-lg shadow-md"></div>
+      <div className="w-full p-6 rounded-lg shadow-md">
+        <h1 className="text-3xl font-semibold text-center text-gray-800">Sign Up</h1>
+        <form className="mt-4">
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Name</span>
+            </label>
+            <input type="text" placeholder="Enter your name" className="w-full input input-bordered h-10" />
+          </div>
+        </form>
+        Z<div></div>
+      </div>
     </div>
   );
 };
