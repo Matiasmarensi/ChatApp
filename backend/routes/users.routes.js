688e03ba4a3ea196886b5c3f297fcd4f39ca2396
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-07-30 18:17:20 -0300

    sign up c/gender

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -3,6 +3,6 @@ import { getUsersSideBar } from "../controllers/users.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 const routes = express.Router();
 
-routes.get("/", protectRoute, getUsersSideBar);
+routes.get("/", protectRoute, rewrwer, getUsersSideBar);
 
 export default routes;
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,19 +1,49 @@
 import React from "react";
+import GenderCheckbox from "./GenderCheckbox";
 
 const SignUp = () => {
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto  ">
-      <div className="w-full p-6 rounded-lg shadow-md">
-        <h1 className="text-3xl font-semibold text-center text-gray-800">Sign Up</h1>
-        <form className="mt-4">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-300 bg-clip-padding background-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-200">
+          Sign Up<span className="text-sky-500"> ChatApp</span>
+        </h1>
+        <form action="">
           <div>
-            <label className="label">
-              <span className="text-base label-text">Name</span>
+            <label className="label p-2">
+              <span className="text-base text-gray-200">Fullname</span>
             </label>
-            <input type="text" placeholder="Enter your name" className="w-full input input-bordered h-10" />
+            <input type="text" placeholder="Matias Marensi" className="w-full input input-bordered h-10 " />
+          </div>
+
+          <div>
+            <label className="label p-2 ">
+              <span className="text-base label-text text-gray-200">Username</span>
+            </label>
+            <input type="text" placeholder="MMarensi" className="w-full input input-bordered h-10" />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text text-gray-200">Password</span>
+            </label>
+            <input type="password" placeholder="Enter password" className="w-full input input-bordered h-10" />
+          </div>
+
+          {/* confirm password */}
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text text-gray-200">Confirm password</span>
+            </label>
+            <input type="password" placeholder="Confirm password" className="w-full input input-bordered h-10" />
+          </div>
+          <GenderCheckbox />
+          <a href="#" className="hover:underline text-sm text-gray-200 inline-block mt-4">
+            Already have an account? Login
+          </a>
+          <div className="flex justify-center mt-4">
+            <button className="btn btn-sm btn-block  border  bg-gray-500 border-slate-700">Sign Up</button>
           </div>
         </form>
-        Z<div></div>
       </div>
     </div>
   );
