2ee64417a4e99339c190f0685c6f9e74f8a54048
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-07-30 19:52:31 -0300

    logout button

--- frontend/src/components/sidebar/SideBar.jsx
+++ frontend/src/components/sidebar/SideBar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import SearchInut from "./SearchInput";
+import SearchInput from "./SearchInput";
 import Conversations from "./Conversations.jsx";
+import LogoutButton from "./LogoutButton.jsx";
+import { TbLogout2 } from "react-icons/tb";
 
 const SideBar = () => {
   return (
-    <div>
-      <SearchInut />
+    <div className="border-r border-slate-500 p-4 flex flex-col">
+      <SearchInput />
       <div className="divider px-3"></div>
       <Conversations />
-      {/* 
-      <LogoutButton /> */}
+      <LogoutButton />
     </div>
   );
 };
