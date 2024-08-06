38bbd7ae7aac6f87e65fc24746ebc578d3baecf1
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-08-06 19:25:45 -0300

    conversations

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Conversation = () => {
+const Conversation = ({ conversation }) => {
   return (
     <>
       <div className="flex gap-3 items-center hover:bg-sky-200 rounded px-2 p-y cursor-pointer">
@@ -11,7 +11,7 @@ const Conversation = () => {
         </div>
         <div className=" flex flex-col  flex-1">
           <div className="flex gap-3 justify-between">
-            <p className="font-bold text-gray-500 ">username</p>
+            <p className="font-bold text-gray-500 ">{conversation.username}</p>
             <span className="text-lg ">emoji</span>
           </div>
         </div>
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Conversation from "./Conversation";
+import useGetConversations from "../../hooks/useGetConversations";
 
 const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+  console.log(conversations);
+
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {loading ? (
+        <div className="loading loading-spinner"></div>
+      ) : (
+        conversations.map((conversation) => <Conversation key={conversation._id} conversation={conversation} />)
+      )}
     </div>
   );
 };
