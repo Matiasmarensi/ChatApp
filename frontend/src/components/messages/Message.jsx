044f9edf23c5e2d21cd9a83a9786b2e14272b432
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-08-07 21:33:31 -0300

    chat

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import { useAuthContext } from "../../context/authContex";
+import useConversation from "../../zustand/useConversation";
 
-const Message = () => {
+const Message = ({ message }) => {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+
+  if (!authUser) return null;
+
+  const fromMe = message.senderId === authUser.user._id;
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  console.log(authUser.user._id);
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "bg-gray-500";
+
+  const formattedTime = new Date(message.createdAt).toLocaleTimeString(); // Formatea la hora correctamente
+
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img src="" alt="avatar" />
+          <img src={profilePic} alt="avatar" />
         </div>
       </div>
-      <div className={"chat-bubble text-white bg-blue-500"}>HOLA !!! </div>
-      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">11:11</div>
+      <div className={`chat-bubble text-white ${bubbleBgColor}`}>{message.message}</div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">{formattedTime}</div>
     </div>
   );
 };
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,26 +1,18 @@
 import React from "react";
 import Message from "./Message";
 import useGetMessage from "../../hooks/useGetMessage";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 
 const Messages = () => {
   const { messages, loading } = useGetMessage();
-  console.log("mensajeeee", messages);
 
   return (
     <div className="px-4 flex-1 overflow-auto ">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-
-      <Message />
-      <Message />
+      {!loading && messages.length > 0 && messages.map((message) => <Message key={message._id} message={message} />)}
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center text-gray-900">Send a message to start the conversation</p>
+      )}
     </div>
   );
 };
