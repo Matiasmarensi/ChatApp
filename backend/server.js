6db0b1311170a9e998373ba7868cdb87cf466954
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-08-15 19:16:59 -0300

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.routes.js";
@@ -8,6 +9,7 @@ import usersRoute from "./routes/users.routes.js";
 import cors from "cors";
 import { app, server } from "./socket/socket.js";
 
+const __dirname = path.resolve();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 app.use(
@@ -16,12 +18,16 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
 app.use("/api/users", usersRoute);
-
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 server.listen(PORT, () => {
   conectToMongodb();
 
